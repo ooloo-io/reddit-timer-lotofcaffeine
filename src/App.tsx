96817bc0c62bd06d2835e30f53440149e712999f
Rana <gutaraposomarques@gmail.com>
2020-09-08 00:41:24 +0200

    feature: update route

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => (
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='search' element={<SearchPage />} />
+        <Route path='search/:query' element={<SearchPage />} />
         <Route path='*' element={<UnkownPage />} />
       </Routes>
     </BrowserRouter>
